bd52ee491dc3902f844d49856f2cf45f9973eb39
kamil t <kamil.t@spoko.pl>
2011-12-07 18:23:43 +0100

    repo testowe2

--- src/kamilnawojka/kamilnawojka.java
+++ src/kamilnawojka/kamilnawojka.java
@@ -14,6 +14,6 @@ public class kamilnawojka {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("repo test");
     }
 }
